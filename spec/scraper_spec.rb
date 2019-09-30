f30b5ca14ef0c6a2288d0934c4530af5dc68a8bc
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-30 01:02:02 +0000

    Done.

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -5,9 +5,9 @@ describe "Scraper" do
 
   let(:scraper) {Scraper.new}
 
-  describe "#get_page" do
-    it "uses Nokogiri to get the HTML from a web page" do
-      doc = scraper.get_page
+  describe "#get_page"   it "uses Nokogiri to get the HTML from a web page" do
+      doc =
+      binding.pry
       expect{scraper.get_page}.to_not raise_error
       expect(doc).to be_a(Nokogiri::HTML::Document)
     end

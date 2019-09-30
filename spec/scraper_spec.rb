352f22cf6ea78af7555de10612f3640fa680d3e2
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-30 00:56:20 +0000

    Automatically backed up by Learn

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -5,9 +5,9 @@ describe "Scraper" do
 
   let(:scraper) {Scraper.new}
 
-  describe "#get_page" do
-    it "uses Nokogiri to get the HTML from a web page" do
-      doc = scraper.get_page
+  describe "#get_page"   it "uses Nokogiri to get the HTML from a web page" do
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      binding.pry
       expect{scraper.get_page}.to_not raise_error
       expect(doc).to be_a(Nokogiri::HTML::Document)
     end
